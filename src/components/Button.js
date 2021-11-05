b070a31eb5e94723e5b37817e96df08357c48c8a
Connie Lai <clai011@ucr.edu>
2021-11-04 21:34:02 -0700

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // props is an 'object' you pass DOWN unidirectionally from parent to child components
-// child component must receive "props" parameter to access data passed down 
+// child component must receive "props" parameter to access data passed down - in this case, parent of this Button component is List component
 const Button=(props)=>{
 
     return (<button>
